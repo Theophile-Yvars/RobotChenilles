11494e7b1803affb90938cd0111c4a77072be313
Theophile-Yvars <theo.yvars@gmail.com>
2026-03-14 19:03:37 +0100

    :bug: image

--- ros2_ws/src/robot_bringup/launch/robot.launch.py
+++ ros2_ws/src/robot_bringup/launch/robot.launch.py
@@ -4,16 +4,14 @@ from launch_ros.actions import Node
 def generate_launch_description():
 
     camera = Node(
-        package='camera_ros',
-        executable='camera_node',
+        package='v4l2_camera',
+        executable='v4l2_camera_node',
         name='camera',
         parameters=[{
-            'camera': 0, 
-            'width': 640,
-            'height': 480,
-            'format': 'BGR888',
-        }],
-        remappings=[('/camera/image_raw', '/image_raw')]
+            'video_device': '/dev/video0',
+            'image_size': [640, 480],
+            'pixel_format': 'YUYV'
+        }]
     )
 
     motors = Node(
