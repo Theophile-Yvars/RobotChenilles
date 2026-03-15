87bfcba3f7ddcbb509c84f785cab97d565ef4bd3
Theophile-Yvars <theo.yvars@gmail.com>
2026-03-15 16:56:50 +0100

    :bug: camera fix (calling main and adding if __name__ == '__main__':)

--- dashboard_web/src/components/Camera.js
+++ dashboard_web/src/components/Camera.js
@@ -6,8 +6,11 @@ function Camera() {
     <div className="video-section">
       <img
         src="http://192.168.1.9:8080/stream?topic=/image_processed"
-        alt="Flux vidéo du robot"
+        alt="Flux vidéo"
         className="video-feed"
+        onError={(e) => {
+          e.target.src = "https://via.placeholder.com/640x480?text=Robot+Hors+Ligne";
+        }}
       />
     </div>
   );
