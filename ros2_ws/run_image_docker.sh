0c68b3dda7aff6ea6adf2bfc270e2a40db30cb63
Theophile-Yvars <theo.yvars@gmail.com>
2026-03-15 16:14:41 +0100

    :rocket: docker

--- ros2_ws/run_image_docker.sh
+++ ros2_ws/run_image_docker.sh
@@ -8,9 +8,15 @@ docker run -dt --name robotchenilles \
   --privileged \
   --net=host \
   --ipc=host \
+  --shm-size=1gb \
   -v /dev:/dev \
   -v /sys:/sys \
   -v /run/udev:/run/udev:ro \
+  -v /run/systemd:/run/systemd:ro \
+  -v /usr/lib/aarch64-linux-gnu/libcamera/:/usr/lib/aarch64-linux-gnu/libcamera/ \
+  -v /usr/lib/aarch64-linux-gnu/libcamera.so.0.5.2:/usr/lib/aarch64-linux-gnu/libcamera.so.0.2 \
+  -v /usr/lib/aarch64-linux-gnu/libcamera-base.so.0.5.2:/usr/lib/aarch64-linux-gnu/libcamera-base.so.0.2 \
+  -v /usr/share/libcamera/:/usr/share/libcamera/ \
   -v ~/robot_ws:/home/robot_ws \
   robot-jazzy-pi5
 
